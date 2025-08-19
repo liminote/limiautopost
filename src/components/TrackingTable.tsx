486f27c22376da2206efbf8e596766db56569b87
Vanny <operatic@Vanny-Mac-mini.local>
2025-08-20 02:33:04 +0800

    調整欄位寬度和對齊方式
    1. 欄位寬度調整：
       - 狀態欄位：從 12ch 減少到 11ch（減少 1ch）
       - 內容欄位：從 15ch 增加到 17ch（增加 2ch）
       - 標籤欄位：從 10ch 減少到 8ch（再減少 2ch）
    
    2. 狀態欄位對齊：
       - 文字從左對齊改為置中對齊
       - 保持字體大小和樣式不變

--- src/components/TrackingTable.tsx
+++ src/components/TrackingTable.tsx
@@ -329,10 +329,10 @@ export default function TrackingTable({ rows, setRows, loading, userEmail }: { r
           <col className="w-8ch" /> {/* 1 原文編號 */}
           <col className="w-8ch" /> {/* 2 識別碼 */}
           <col className="w-9ch" /> {/* 3 平台 */}
-          <col className="w-12ch" /> {/* 4 狀態 - 加寬 4ch */}
+          <col className="w-11ch" /> {/* 4 狀態 - 減少 1ch */}
           <col className="w-12ch" /> {/* 5 原文標題 */}
-          <col className="w-15ch" /> {/* 6 內容 */}
-          <col className="w-10ch" /> {/* 7 標籤 - 縮小 4ch */}
+          <col className="w-17ch" /> {/* 6 內容 - 增加 2ch */}
+          <col className="w-8ch" /> {/* 7 標籤 - 再減少 2ch */}
           <col style={{ width: '10ch' }} /> {/* 8 連結 */}
           <col className="w-12ch" /> {/* 9 發佈日期 */}
           <col style={{ width: '8ch' }} /> {/* 10 讚 */}
